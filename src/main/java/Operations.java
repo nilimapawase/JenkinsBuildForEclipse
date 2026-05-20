a922fb47d51a9a10b11c207a96f84a938ba77bdd
Nilima Pawase <158264249+nilimapawase@users.noreply.github.com>
2026-05-20 17:10:37 +0530

    a

--- src/main/java/Operations.java
+++ src/main/java/Operations.java
@@ -3,15 +3,15 @@ public class Operations {
 	
 	public void Add()
 	{
-		int a=20,b=10, c
+		int a=20,b=10, c;
 		c=a+b;
-		System.out.println("Addition is= "+c)
+		System.out.println("Addition is= "+c;
 	}
 	public void Sub()
 	{
 		int a=20,b=10, c;
 		c=a-b;
-		System.out.println("Substraction is= "+c)
+		System.out.println("Substraction is= "+c);
 	}
 
 	public static void main(String[] args) {
