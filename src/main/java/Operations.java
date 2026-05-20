2528d005e2036f584f1cc32db9028258c59772a4
nilimapawase <nilimapawase14@gmail.com>
2026-05-20 12:46:14 +0530

    Initial commit

---
+++ src/main/java/Operations.java
@@ -0,0 +1,26 @@
+
+public class Operations {
+	
+	public void Add()
+	{
+		int a=20,b=10, c;
+		c=a+b;
+		System.out.println("Addition is= "+c);
+	}
+	public void Sub()
+	{
+		int a=20,b=10, c;
+		c=a-b;
+		System.out.println("Substraction is= "+c);
+	}
+
+	public static void main(String[] args) {
+
+		Operations o=new Operations();
+		o.Add();
+		o.Sub();
+		
+		
+	}
+
+}
